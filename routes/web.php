4efeb3dea10091c84648b8cf172c22f728d73d46
nvoostrom <nick.v.oostrom+github@gmail.com>
2023-04-06 17:49:31 +0200

    add databases

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\contactNoteController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,20 +22,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', AppController::class);
 
-Route::controller(ContactController::class)->prefix('admin')->name('contacts.')->group(function(){
+Route::controller(ContactController::class)->prefix('admin')->name('contacts.')->group(function () {
     Route::get('/contacts', 'index')->name('index');
     Route::get('/contacts/create', 'create')->name('create');
     Route::get('/contact/edit/{id}', 'edit')->name('edit');
     Route::get('/contacts/{id}', 'show')->whereNumber('id')->name('show');
 });
 
-Route::get('/company/{name?}', function ($name = null) {
-    if ($name) {
-        return '<h1>Hello world</h1> <h2>Here you can view a company</h2> <h3> this company name is ' . $name . '</h3>';
-    } else {
-        return '<h1>Hello world</h1> <h2>Here you can view all companies</h2>';
-    }
-})->whereAlpha('name');
+Route::resource('companies', CompanyController::class);
+
+Route::resources([
+    '/tags' => TagController::class,
+    '/tasks' => TaskController::class,
+    // '/companies' => CompanyController::class,
+]);
+
+// Route::resource('/activity', ActivityController::class)->names(['index' => 'activity.all', 'show' => 'activity.view']);
+Route::resource('/activity', ActivityController::class)->parameters(['activity' => 'active', 'show' => 'activity.view']);
+
+
+Route::resource('/contact.notes', contactNoteController::class)->shallow();
+
 
 
 Route::fallback(
